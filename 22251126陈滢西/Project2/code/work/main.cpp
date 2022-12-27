201f73c695f8c50405b79be9743324123bb7f760
chen-yingxi <510872590@qq.com>
2022-12-27 15:20:49 +0800

    p2

---
+++ 22251126陈滢西/Project2/code/work/main.cpp
@@ -0,0 +1,214 @@
+﻿#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <SOIL2\soil2.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <stack>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp> // glm::value_ptr
+#include <glm\gtc\matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include "Utils.h"
+#include "Sphere.h"
+using namespace std;
+
+#define numVAOs 1
+#define numVBOs 3
+
+float cameraX, cameraY, cameraZ;
+float sphLocX, sphLocY, sphLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+GLuint earthTexture;
+GLuint moonTexture;
+GLuint sunTexture;
+float rotAmt = 0.0f;
+
+// variable allocation for display
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+stack<glm::mat4> mvStack;
+Sphere mySphere = Sphere(48);
+
+void setupVertices(void) {
+	std::vector<int> ind = mySphere.getIndices();
+	std::vector<glm::vec3> vert = mySphere.getVertices();
+	std::vector<glm::vec2> tex = mySphere.getTexCoords();
+	std::vector<glm::vec3> norm = mySphere.getNormals();
+
+	std::vector<float> pvalues;	//顶点位置
+	std::vector<float> tvalues;	//纹理坐标
+	std::vector<float> nvalues;	//法向量
+
+	int numIndices = mySphere.getNumIndices();
+	for (int i = 0; i < numIndices; i++) {
+		pvalues.push_back((vert[ind[i]]).x);
+		pvalues.push_back((vert[ind[i]]).y);
+		pvalues.push_back((vert[ind[i]]).z);
+		tvalues.push_back((tex[ind[i]]).s);
+		tvalues.push_back((tex[ind[i]]).t);
+		nvalues.push_back((norm[ind[i]]).x);
+		nvalues.push_back((norm[ind[i]]).y);
+		nvalues.push_back((norm[ind[i]]).z);
+	}
+
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, pvalues.size() * 4, &pvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, tvalues.size() * 4, &tvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+	glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 2.0f;
+	sphLocX = 0.0f; sphLocY = 0.0f; sphLocZ = -1.0f;
+
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+
+	setupVertices();
+	sunTexture = Utils::loadTexture("sun.jpg");
+	earthTexture = Utils::loadTexture("earth.jpg");
+	moonTexture = Utils::loadTexture("moon.png");
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");//返回查询的统一变量的位置
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mvStack.push(vMat);
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	// ----------------------sun
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	mvStack.push(mvStack.top());
+	mvStack.top() *= rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0, 1.0, 0.0));
+	mvStack.top() *= scale(glm::mat4(1.0f), glm::vec3(0.25f, 0.25f, 0.25f));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, sunTexture);
+
+	glEnable(GL_CULL_FACE);		//调整深度
+	glFrontFace(GL_CCW);
+
+	glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
+	mvStack.pop();
+
+	//-----------------------planet  
+
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(sin((float)currentTime) * 1.0, 0.0f, cos((float)currentTime) * 1.0));
+	mvStack.push(mvStack.top());
+	mvStack.top() *= rotate(glm::mat4(1.0f), (float)currentTime * 3, glm::vec3(0.0, 1.0, 0.0));
+	mvStack.top() *= scale(glm::mat4(1.0f), glm::vec3(0.1f, 0.1f, 0.1f));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, earthTexture);
+
+	glEnable(GL_CULL_FACE);		//调整深度
+	glFrontFace(GL_CCW);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
+	mvStack.pop();
+
+	//-----------------------moon
+	mvStack.push(mvStack.top());
+	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(sin((float)currentTime * 7) * 0.15, 0.0f, cos((float)currentTime * 7) * 0.15));
+	mvStack.top() *= rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0, 0.0, 1.0));
+	mvStack.top() *= scale(glm::mat4(1.0f), glm::vec3(0.02f, 0.02f, 0.02f));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, moonTexture);
+
+	glEnable(GL_CULL_FACE);		//调整深度
+	glFrontFace(GL_CCW);
+
+	glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
+	mvStack.pop();
+}
+
+void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
+	aspect = (float)newWidth / (float)newHeight;
+	glViewport(0, 0, newWidth, newHeight);
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+}
+
+int main(void) {
+	glewExperimental = GL_TRUE;
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(1000, 600, "test", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+	
+	glfwSetWindowSizeCallback(window, window_size_callback);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
