7f42f4acbdf064d13cccd5b761b385b515907b6f
LS5221 <96706261+LS5221@users.noreply.github.com>
2022-12-31 13:47:17 +0800

    Delete 22251082梁书伟 directory

--- 22251082梁书伟/作业2/SolarSystem-main/SolarSystem-main/src/file.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def resolve(filename):
-    """
-    Resolves file path independent of path of command execution 
-    """
-    return '{0}\\assets\{1}'.format(os.path.dirname(os.path.abspath(__file__)), filename)
